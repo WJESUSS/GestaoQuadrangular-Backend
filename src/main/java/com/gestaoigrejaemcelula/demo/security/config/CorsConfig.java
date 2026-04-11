0f808a7d8149abd03c68e139a4e981d3bf65c4ca
wash <washquesia@gmail.com>
2026-04-11 11:51:15 -0300

    atualizacao cors

--- src/main/java/com/gestaoigrejaemcelula/demo/security/config/CorsConfig.java
+++ src/main/java/com/gestaoigrejaemcelula/demo/security/config/CorsConfig.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class CorsConfig {
 
 
-    @Value("${CORS_ALLOWED_ORIGINS:http://localhost:5173,http://localhost:3000}")
+    @Value("${CORS_ALLOWED_ORIGINS=http://localhost:5173,http://localhost:3000,https://gestaoquadrangularpituacubr.vercel.app}")
 
     private String allowedOriginsRaw;
 
