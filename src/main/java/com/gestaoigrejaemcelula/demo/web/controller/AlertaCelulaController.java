5d5a79d117bb3b1247de429ae16dcf9a02075e18
wash <washquesia@gmail.com>
2026-04-28 23:58:37 -0300

    Teste 01

--- src/main/java/com/gestaoigrejaemcelula/demo/web/controller/AlertaCelulaController.java
+++ src/main/java/com/gestaoigrejaemcelula/demo/web/controller/AlertaCelulaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/alertas-celulas")
+@RequestMapping("/alertas-celulas")
 @RequiredArgsConstructor
 public class AlertaCelulaController {
 
